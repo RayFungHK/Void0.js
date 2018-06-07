dca7d5a34b53548715ef19a9e0299b4660ff3f2a
Ray Fung <hello@rayfung.hk>
2018-06-07 18:19:31 +0800

    v0.01 build 10 Start writing Moduler.Ajax

--- src/Moduler.js
+++ src/Moduler.js
@@ -1,14 +1,13 @@
 (function(global) {
 	"use strict";
   var fn = {},
-      ary = Array.prototype,
-      Package = {},
       configuration = {
         plugin: './plugin/'
       },
       allType = '*/'.concat('*'),
 
       // Shortern Function
+		  ary = Array.prototype,
       toString = Object.prototype.toString,
       win = window,
       doc = document,
@@ -17,45 +16,80 @@
   		slice = ary.slice,
   		some = ary.some,
 
+			container = doc.createElement('div'),
+
       // Regex
       regexConstructor = /^\[object .+?Constructor\]$/,
       regexNative = new RegExp('^' + String(toString).replace(/[.*+?^${}()|[\]\/\\]/g, '\\$&').replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, '$1.*?') + '$');
 
-  Package.prototype = {
-		constructor: Package,
-		push: ary.push,
-		indexOf: ary.indexOf,
-		forEach: ary.forEach,
-		some: ary.some,
-		length: 0
-	};
+	/**
+	 * Moduler
+	 * @constructor
+	 * return ElementCollection
+	 */
+  function Moduler(object) {
+		if (fn.isCallable(object)) {
+			return this;
+		} else {
+			var collection = new ElementCollection();
+			collectElements(object, doc, collection);
 
-  var Moduler = function(selector) {
+			// Clear _added flag
+			fn.each(collection, function() {
+				delete this._added;
+			});
 
+			return collection;
+		}
   };
 
   // Moduler Function
   fn = {
+		/**
+		 * [description]
+		 * @param  {object} object [description]
+		 * @return {boolean}        [description]
+		 */
 		isDefined: function(object) {
 			return (typeof object != 'undefined');
 		},
 
+		/**
+		 * [description]
+		 * @param  {[type]} object [description]
+		 * @return {[type]}        [description]
+		 */
 		isNative: function(object) {
 			var type = typeof object;
 			return (type === 'function') ? regexNative.test(Function.prototype.toString.call(object)) : (object && type === 'object' && regexConstructor.test(toString.call(object))) || false;
 		},
 
+		/**
+		 * [description]
+		 * @param  {[type]} object [description]
+		 * @return {[type]}        [description]
+		 */
 		isCallable: function(object) {
 			return (typeof object == 'function');
 		},
 
+		/**
+		 * [description]
+		 * @param  {[type]} object [description]
+		 * @return {[type]}        [description]
+		 */
 		isObject: function(object) {
 			return typeof object === 'object';
 		},
 
+		/**
+		 * [description]
+		 * @param  {[type]} object [description]
+		 * @return {[type]}        [description]
+		 */
 		isPlainObject: function(object) {
 			if (object !== null && fn.isObject(object)) {
-				if (fn.IsCallable(Object.getPrototypeOf)) {
+				if (fn.isCallable(Object.getPrototypeOf)) {
 					var proto = Object.getPrototypeOf(object);
 					return proto === Object.prototype || proto === null;
 				}
@@ -64,10 +98,48 @@
 			return false;
 		},
 
+		/**
+		 * [description]
+		 * @param  {[type]} object [description]
+		 * @return {[type]}        [description]
+		 */
+		isIterable: function(object) {
+			return (object && (fn.isNative(object.some) || fn.isNative(object.forEach)));
+		},
+
+		/**
+		 * [description]
+		 * @param  {[type]} object [description]
+		 * @return {[type]}        [description]
+		 */
+		isString: function(object) {
+			return (typeof object === 'string');
+		},
+
+		/**
+		 * [description]
+		 * @param  {[type]} object [description]
+		 * @return {[type]}        [description]
+		 */
 		isArray: function(object) {
 			return (object && Array.isArray(object));
 		},
 
+		/**
+		 * [description]
+		 * @param  {[type]} object [description]
+		 * @return {[type]}        [description]
+		 */
+		isDOMElement: function(object) {
+			return object && (object.nodeType == 1 || object.nodeType == 9 || object.nodeType == 11);
+		},
+
+		/**
+		 * [description]
+		 * @param  {[type]}   object   [description]
+		 * @param  {Function} callback [description]
+		 * @return {[type]}            [description]
+		 */
     each: function(object, callback) {
   		if (fn.isNative(object.some)) {
   			object.some(function(element, index, object) {
@@ -96,22 +168,26 @@
   		}
     },
 
+		/**
+		 * [description]
+		 * @param  {[type]} object       [description]
+		 * @param  {[type]} extendObject [description]
+		 * @return {[type]}              [description]
+		 */
     extend: function(object, extendObject) {
-  		var args = slice.call(arguments);
-  		args.shift();
-
-  		fn.each(args, function() {
-  			if (typeof this == 'object' && this !== null) {
-  				fn.each(this, function(key, val) {
-  					if (typeof object[key] == 'undefined') {
-  						object[key] = val;
-  					}
-  				});
-  			}
+  		fn.each(extendObject, function(key, val) {
+				if (!fn.isDefined(object[key])) {
+					object[key] = val;
+				}
   		});
   		return this;
   	},
 
+		/**
+		 * [description]
+		 * @param  {[type]} object [description]
+		 * @return {[type]}        [description]
+		 */
 		clone: function(object) {
 			var newObject;
 
@@ -141,12 +217,87 @@
 		}
   };
 
-  Moduler = fn.extend(Moduler, fn);
+  fn.extend(Moduler, fn);
+
+	// ElementCollection
+	// Store all elements and provides ton of function
+	var ElementCollection = (function() {
+		/**
+		 * [ElementCollectionClass description]
+		 * @param       {[type]} elements [description]
+		 * @constructor
+		 */
+		function ElementCollectionClass(elements) {
+			var self = this;
+			if (fn.isDefined(elements)) {
+				if (fn.isIterable(elements)) {
+					fn.each(elements, function() {
+						if (fn.isDOMElement(this)) {
+							self.push(this);
+						}
+					});
+				} else if (fn.isDOMElement(elements)) {
+					self.push(elements);
+				}
+			}
+		}
+
+	  ElementCollectionClass.prototype = {
+			constructor: ElementCollectionClass,
+			push: function(element) {
+				if (!fn.isDOMElement(element)) {
+					throw new TypeError('Object ' + element + ' is not an element.');
+				}
+				ary.push.call(this, element);
+			},
+			indexOf: ary.indexOf,
+			forEach: ary.forEach,
+			some: ary.some,
+			length: 0
+		};
+
+		return ElementCollectionClass;
+	})();
+
+	function collectElements(selector, context, source) {
+		context = (context && context.nodeType && context.querySelectorAll) ? context : doc;
+		if (fn.isString(selector)) {
+			selector = selector.trim();
+			if (selector) {
+				// If selector is a DOM string
+				if (/^<.+>$/.test(selector)) {
+					container.innerHTML = selector;
+					fn.each(container.children, function() {
+						source.push(this);
+					});
+					container.innerHTML = '';
+				} else {
+					fn.each(context.querySelectorAll(selector), function() {
+						if (!this._added) {
+							this._added = true;
+							source.push(this);
+						}
+					});
+				}
+			}
+		} else if (selector instanceof ElementCollection || fn.isIterable(selector)) {
+			fn.each(selector, function() {
+				collectElements(this, context, source);
+			});
+		} else if (fn.isDOMElement(selector) || selector === win || selector === doc) {
+			if (!this._added) {
+				selector._added = true;
+				source.push(selector);
+			}
+		}
+
+		return source;
+	}
 
   function select(needle, object) {
     var packer = {};
 
-    packer.then = function(callback) {
+    packer.each = function(callback) {
       if (fn.isCallable(callback)) {
         for (var key in object) {
           callback.apply(object, [key, object[key]]);
@@ -158,10 +309,15 @@
     return packer;
   }
 
-  // Promises/A+
-  // An open standard for sound, interoperable JavaScript promises
-  // https://promisesaplus.com/
+	// [ Moduler.Promise ]
+  // 		Abide by Promises/A+ Rule
+  // 		https://promisesaplus.com/
   Moduler.Promise = (function() {
+		/**
+		 * [PromiseClass description]
+		 * @param       {[type]} executor [description]
+		 * @constructor
+		 */
     function PromiseClass(executor) {
 			var promise = this;
 
@@ -192,6 +348,12 @@
       }
 		}
 
+		/**
+		 * [description]
+		 * @param  {[type]} onFulfilled [description]
+		 * @param  {[type]} onRejected  [description]
+		 * @return {[type]}             [description]
+		 */
     PromiseClass.prototype.then = function(onFulfilled, onRejected) {
       var promise = this,
 					task = {
@@ -221,6 +383,11 @@
       return task.promise;
     };
 
+		/**
+		 * [description]
+		 * @param  {[type]} mixed [description]
+		 * @return {[type]}       [description]
+		 */
 		PromiseClass.resolve = function(mixed) {
 			if (mixed instanceof PromiseClass) {
 				return mixed;
@@ -231,6 +398,11 @@
 			});
 		};
 
+		/**
+		 * [description]
+		 * @param  {[type]} reason [description]
+		 * @return {[type]}        [description]
+		 */
 		PromiseClass.reject = function(reason) {
 			if (reason instanceof PromiseClass) {
 				return reason;
@@ -391,9 +563,299 @@
 		return PromiseClass;
 	})();
 
+	Moduler.Ajax = (function() {
+		var ajaxSettings = {
+				cached: {},
+				default: {
+					url: location.href,
+					type: 'GET',
+					processData: true,
+					async: true,
+					contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+
+					/* Default Null */
+					timeout: 0,
+					data: null,
+					dataType: null,
+					username: null,
+					password: null,
+					cache: null,
+					headers: {},
+
+					accepts: {
+						'*': allType,
+						text: 'text/plain',
+						html: 'text/html',
+						xml: 'application/xml, text/xml',
+						json: 'application/json, text/javascript'
+					},
+
+					converters: {
+						'* text': win.String,
+						'text html': true,
+						'text json': fn.parseJSON,
+						'text xml': fn.parseXML
+					}
+				}
+			};
+		ajaxSettings._default = fn.clone(ajaxSettings.default);
+
+		function AjaxClass(url, settings) {
+			if (fn.isPlainObject(url)) {
+				settings = url;
+				url = settings.url;
+			}
+
+			if (!fn.isPlainObject(settings)) {
+				settings = ajaxSettings.default;
+			} else {
+
+				fn.each(ajaxSettings.default, function(key, val) {
+					if (key != 'headers' && key != 'accepts' && key != 'converters') {
+						if (!fn.isDefined(settings[key])) {
+							settings[key] = val;
+						}
+					}
+				});
+			}
+			url = (fn.isString(url)) ? url : ajaxSettings.default.url;
+
+			var promise = new Moduler.Promise(function(resolve, reject) {
+				var xmlHttp = null,
+						action = this,
+						converters = {};
+
+				// settings.beforeSend
+				if (fn.isCallable(settings.beforeSend)) {
+					settings.beforeSend.call(xmlHttp, xmlHttp);
+				}
+
+				// settings.crossDomain
+				if (settings.crossDomain || settings.dataType === 'script') {
+					if (settings.dataType !== 'script') {
+						// settings.jsonp
+						var jsonpFunc = '';
+						if (settings.jsonp && fn.isString(settings.jsonp)) {
+							jsonpFunc = settings.jsonp;
+						} else {
+							jsonpFunc = Math.random().toString(36).replace(/\d/g, '').slice(2, 7);
+							if (!fn.isDefined(win._ajaxCallback)) {
+								win._ajaxCallback = {};
+							}
+
+							win._ajaxCallback[jsonpFunc] = function(data) {
+								if (fn.isCallable(settings.jsonpCallback)) {
+									settings.jsonpCallback(data);
+								}
+								resolve(data);
+							};
+
+							jsonpFunc = 'window._ajaxCallback.' + jsonpFunc;
+						}
+						url = url + ((/\?/).test(url) ? '&' : '?') + 'callback=' + jsonpFunc;
+					}
+
+					var script = doc.createElement('script');
+					// settings.data
+					if (settings.data) {
+						settings.data = fn.param(settings.data);
+						url += '&' + settings.data;
+					}
+
+					// settings.scriptCharset
+					if (settings.scriptCharset) {
+						script.charset = settings.scriptCharset;
+					}
+
+					script.src = url;
+					doc.getElementsByTagName('head')[0].appendChild(tag);
+					script.onload = function() {
+						onCompleted();
+					};
+
+					script.onerror = function(e) {
+						reject(e);
+					};
+				} else {
+					xmlHttp = new XMLHttpRequest();
+					// settings.method
+					if (!settings.method) {
+						settings.method = settings.method || settings.type || 'GET';
+					}
+					settings.method = settings.method.toUpperCase();
+
+					// settings.data
+					if (settings.data) {
+						if (settings.processData && !fn.isString(settings.data) && (settings.method == 'GET' || settings.data.constructor != FormData)) {
+							settings.data = fn.param(settings.data);
+						}
+						if (settings.method == 'GET') {
+							url += ((/\?/).test(url) ? '&' : '?') + settings.data;
+						}
+					}
+
+					// settings.cache
+					if (!settings.cache && (!settings.dataType || settings.dataType == 'jsonp' || settings.dataType == 'script')) {
+						url = url + ((/\?/).test(url) ? '&' : '?') + (new Date()).getTime();
+					}
+
+					if (!fn.isDefined(settings.async)) {
+						settings.async = true;
+					}
+					xmlHttp.open(settings.method, url, settings.async, settings.username, settings.password);
+
+					// settings.timeout
+					if (parseInt(settings.timeout) > 0) {
+						xmlHttp.timeoutTimer = setTimeout(function() {
+							xmlHttp.abort('timeout');
+							reject('timeout');
+						}, parseInt(settings.timeout));
+					}
+
+					// settings.accepts
+					if (!fn.isPlainObject(settings.accepts)) {
+						settings.accepts = {};
+					}
+						console.log(xmlHttp);
+					fn.extend(settings.accepts, ajaxSettings.default.accepts);
+					xmlHttp.setRequestHeader('Accept', (settings.dataType && settings.accepts[settings.dataType]) ? settings.accepts[settings.dataType] + ((settings.dataType !== '*') ? ', ' + allType + '; q=0.01' : '') : settings.accepts['*']);
+
+					// settings.contentType
+					if (settings.data && settings.data.constructor == FormData) {
+						settings.contentType = 'multipart/form-data; charset=UTF-8';
+					} else {
+						if (!fn.isDefined(settings.contentType)) {
+							settings.contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
+						}
+						if (settings.contentType !== false) {
+							xmlHttp.setRequestHeader('Content-Type', settings.contentType);
+						}
+					}
+
+					// settings.converters
+					if (fn.isPlainObject(settings.converters)) {
+						fn.each(settings.converters, function(name, callback) {
+							name = name.trim();
+							if (/[\w-]+\s[\w-]/.test(name) && (fn.isCallable(callback) || callback === true)) {
+								converters[name] = callback;
+							}
+						});
+					}
+					fn.extend(converters, ajaxSettings.default.converters);
+
+					// settings.headers
+					if (fn.isPlainObject(settings.headers)) {
+						fn.each(settings.headers, function(name, value) {
+							xmlHttp.setRequestHeader(name.trim(), value);
+						});
+					}
+
+					// settings.mimeType
+					if (settings.mimeType && fn.isString(settings.mimeType)) {
+						xmlHttp.overrideMimeType(settings.mimeType);
+					}
+
+					xmlHttp.onreadystatechange = function() {
+						if (xmlHttp.readyState != 4) return;
+
+						// settings.statusCallback
+						if (fn.isCallable(settings.statusCallback)) {
+							if (settings.statusCallback[xmlHttp.status]) {
+								settings.statusCallback[xmlHttp.status].call(xmlHttp);
+							}
+						}
+
+						if (xmlHttp.status == 200) {
+							var header = xmlHttp.getResponseHeader('Content-Type'),
+								delimited = header.split(';')[0].split('/'),
+								contentType = delimited[0],
+								outputFormat = delimited[1],
+								response = xmlHttp.response,
+								convertName = contentType + ' ' + ((settings.dataType) ? settings.dataType : outputFormat),
+								modifiedCheck = {};
+
+							// settings.ifModified
+							if (settings.ifModified) {
+								modifiedCheck.etag = xmlHttp.getResponseHeader('ETag');
+								modifiedCheck.lastModified = xmlHttp.getResponseHeader('Last-Modified');
+								if (ajaxSettings.cached[url]) {
+									if (ajaxSettings.cached[url].lastModified != modifiedCheck.lastModified || (modifiedCheck.etag && ajaxSettings.cached[url].eTag == modifiedCheck.etag)) {
+										ajaxSettings.cached[url] = modifiedCheck;
+									} else {
+										reject({
+											status: 304,
+											text: 'Not modified'
+										});
+										onCompleted();
+										return;
+									}
+								}
+							}
+
+							// settings.afterDataReceive
+							if (fn.isCallable(settings.afterDataReceive)) {
+								response = settings.afterDataReceive.call(response, response);
+							}
+
+							if (converters[convertName]) {
+								if (converters[convertName] !== true) {
+									response = converters[convertName](response);
+								}
+							}
+
+							resolve(response);
+							onCompleted();
+						} else {
+							reject({
+								status: xmlHttp.status,
+								text: xmlHttp.statusText
+							});
+							onCompleted();
+						}
+					};
+
+					xmlHttp.send(settings.data);
+				}
+			});
+
+			function onCompleted() {
+				// settings.complete
+				if (!fn.isIterable(settings.complete)) {
+					settings.complete = [settings.complete];
+				}
+
+				fn.each(settings.complete, function() {
+					if (fn.isCallable(this)) {
+						this.call(this, xmlHttp, xmlHttp.statusText);
+					}
+				});
+			}
+
+			return promise;
+		}
+
+		AjaxClass.config = function(param, value) {
+			if (fn.isPlainObject(param)) {
+				fn.each(param, this);
+			} else {
+				if (fn.isDefined(ajaxSettings.default[param])) {
+					ajaxSettings.default[param] = value;
+				}
+			}
+			return this;
+		};
+
+		AjaxClass.reset = function() {
+			ajaxSettings.default = fn.clone(ajaxSettings._default);
+			return this;
+		};
+
+		return AjaxClass;
+	})();
+
   Moduler.config = function(config) {
     if (typeof config !== undefined) {
-      select('plugin'.split(' '), config).then(function(parameter, value) {
+      select('plugin'.split(' '), config).each(function(parameter, value) {
         if (parameter === 'plugin') {
           value += '/';
           var matches = /^(?:.{0,2}\/+)?(?:[\w-.]+\/*)+(\/)+?$/.exec(value)
@@ -434,17 +896,14 @@
       script.charset = 'utf-8';
       script.type = 'text/javascript';
 
-      console.log(1);
-
       script.src = configuration.plugin + scriptName;
 
       head.appendChild(script);
-      console.log(3);
     }
     return this.exports;
   };
 
-  Moduler.exports = null;
+	global.Moduler = Moduler;
 
-  global.Moduler = Moduler;
+  Moduler.exports = null;
 })(window);
